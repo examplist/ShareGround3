f3efb599fb62b083e12c7da52a45c6decc74c949
examplist <iykl3670@gmail.com>
2023-08-06 14:13:45 +0900

    🐛 CORS 해결 위해 webconfig 추가

---
+++ final-back/src/main/java/base/WebConfig.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*").allowCredentials(false);
+	}
+}
